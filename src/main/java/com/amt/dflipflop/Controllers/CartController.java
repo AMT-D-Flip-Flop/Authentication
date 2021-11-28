8c79a0bea606641a9cb9f1950e5b6caa0b81f65e
michael <michael.ruckstuhl@heig-vd.ch>
2021-11-28 18:59:13 +0100

    [WIP] CAN CHANGE NUMBER AND DELETE IF 0, DO NOT USE DATABASE

--- src/main/java/com/amt/dflipflop/Controllers/CartController.java
+++ src/main/java/com/amt/dflipflop/Controllers/CartController.java
@@ -21,17 +21,16 @@ public class CartController {
     @Autowired
     private ProductService productService;
 
-    private boolean init = false;
-    private Integer i = 0;
+    private boolean init = true;
     private List<ItemCart> itemCarts = new ArrayList<>();
 
 
     @GetMapping("/cart")
     public String getCart(Model model) {
         if(!init){
+            itemCarts.clear();
             init = true;
             Cart cart = new Cart();
-            cartService.insert(cart);
             Product product1 = new Product("Produit 1", "Super produit 1", 3.5f, "shoes-img3.png");
             Product product2 = new Product("Produit 2", "Super produit 2", 6.5f, "shoes-img9.png");
             Product product3 = new Product("Produit 3", "Super produit 3", 9.5f, "shoes-img4.png");
@@ -42,8 +41,6 @@ public class CartController {
             cart.addProduct(item2);
             cart.addProduct(item3);
             itemCarts = cart.getItemsCart();
-        } else {
-            ++i;
         }
         double total = 0;
         for(ItemCart ic : itemCarts)
@@ -53,15 +50,25 @@ public class CartController {
         return "cart";
     }
 
-    @PostMapping("/cart/change")
-    public String addQty(@ModelAttribute ("itemCart") ItemCart itemCart) {
-        System.out.println(itemCart.getQty());
+    @PostMapping("/cart/change/{id}")
+    public String addQty(@ModelAttribute ("itemCart") ItemCart itemCart, @PathVariable("id") int index) {
+        if(itemCart.getQty() > 0){
+            itemCarts.get(index).setQty(itemCart.getQty());
+        } else {
+            itemCarts.remove(index);
+        }
         return "redirect:/cart";
     }
 
     @PostMapping("/cart/empty")
     public String empty() {
         itemCarts.clear();
+        return "redirect:/cart";
+    }
+
+    @PostMapping("/cart/init")
+    public String init() {
+        init = false;
         return "redirect:/cart";
     }
 
