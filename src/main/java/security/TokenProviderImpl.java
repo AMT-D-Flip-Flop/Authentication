64d62f9a7ac62855e1207a9f916a24dccec287fd
rya-sge <ryan.sauge@heig-vd.ch>
2021-12-10 17:16:20 +0100

    add key reader

--- src/main/java/security/TokenProviderImpl.java
+++ src/main/java/security/TokenProviderImpl.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -25,6 +29,10 @@ public class TokenProviderImpl implements TokenProvider {
 
     //@Value("${authentication-test.auth.tokenSecret}")
     private String tokenSecret = "secret";
+    private boolean keyGenerated = false;
+
+    @Value("${mode}")
+    private String mode;
 
     @Value("${authentication-test.auth.tokenExpirationMsec}")
     private Long tokenExpirationMsec;
@@ -34,8 +42,44 @@ public class TokenProviderImpl implements TokenProvider {
 
     private LinkedHashMap lp;
 
+    //Read file content into the string with - Files.lines(Path path, Charset cs)
+
+    private static String readLine(String filePath) throws IOException {
+        // File path is passed as parameter
+        File file = new File(
+                filePath);
+
+        // Note:  Double backquote is to avoid compiler
+        // interpret words
+        // like \test as \t (ie. as a escape sequence)
+
+        // Creating an object of BufferedReader class
+        BufferedReader br
+                = new BufferedReader(new FileReader(file));
+
+        // Declaring a string variable
+        String st = br.readLine();
+        // Consition holds true till
+        // there is character in a string
+        return st;
+    }
+
+    void generateKey() {
+        if(!keyGenerated && mode.equals("prod")){
+            try {
+                tokenSecret = readLine(        "zone_secret/jwt.txt");
+            }catch(Exception e){
+                //
+            }
+
+            keyGenerated = true;
+        }
+
+    }
+
     @Override
     public Token generateAccessToken(String subject) {
+        generateKey();
         Date now = new Date();
         Long duration = now.getTime() + tokenExpirationMsec;
         Date expiryDate = new Date(duration);
@@ -50,6 +94,7 @@ public class TokenProviderImpl implements TokenProvider {
 
     @Override
     public Token generateRefreshToken(String subject) {
+        generateKey();
         Date now = new Date();
         Long duration = now.getTime() + refreshTokenExpirationMsec;
         Date expiryDate = new Date(duration);
@@ -63,6 +108,7 @@ public class TokenProviderImpl implements TokenProvider {
     }
     @Override
     public HashMap getAccountFromToken(String token){
+        generateKey();
         Claims claims = Jwts.parser().setSigningKey(tokenSecret.getBytes(Charset.forName("UTF-8"))).parseClaimsJws(token).getBody();
         lp = (LinkedHashMap) claims.get("account");
         return lp;
@@ -83,12 +129,14 @@ public class TokenProviderImpl implements TokenProvider {
 
     @Override
     public LocalDateTime getExpiryDateFromToken(String token) {
+        generateKey();
         Claims claims = Jwts.parser().setSigningKey(tokenSecret).parseClaimsJws(token).getBody();
         return LocalDateTime.ofInstant(claims.getExpiration().toInstant(), ZoneId.systemDefault());
     }
 
     @Override
     public boolean validateToken(String token) throws Exception {
+        generateKey();
         try {
             /*
             https://stackoverflow.com/questions/65306718/io-jsonwebtoken-signatureexception-jwt-signature-does-not-match-locally-compute
