4e680678d81c316c7d3655bcb2b8f56cad4e650e
TheSnekySnek <diegovillagrasa@gmail.com>
2021-12-13 13:49:19 +0100

    Removed unused code

--- src/main/java/com/amt/dflipflop/Controllers/CartController.java
+++ src/main/java/com/amt/dflipflop/Controllers/CartController.java
@@ -11,11 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-import static java.lang.Integer.parseInt;
 
 @Controller
 public class CartController {
